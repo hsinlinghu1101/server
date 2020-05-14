a053ef45a9ccaf70cc3c3e279b2454aaba229182
Hsin Ling Hu <hsinkava11@gmail.com>
2020-05-13 20:41:11 -0700

    add api

--- src/modules/app.js
+++ src/modules/app.js
@@ -9,8 +9,8 @@ app.use(cors({
   origin: CLIENT_ORIGIN
 }));
 
-app.use('/people', require('./people/people.router'));
-app.use('/pets', require('./pets/pets.router'));
+app.use('/api/people', require('./people/people.router'));
+app.use('/api/pets', require('./pets/pets.router'));
 
 app.get('/', (req, res) => {
   res.send(store);
